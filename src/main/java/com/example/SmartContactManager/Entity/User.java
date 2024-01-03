6662eda2071aeecf3f04fbb9aa503956af3df798
RAJ_SRI_GULSHAN <rajgulshan20022000@gmail.com>
2024-01-04 02:32:20 +0530

    Initial commit to feature of smart contact mamanger

---
+++ src/main/java/com/example/SmartContactManager/Entity/User.java
@@ -0,0 +1,27 @@
+package com.example.SmartContactManager.Entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="USER")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long    id;
+    private String  name;
+    @Column(unique = true)
+    private String  email;
+    private String  password;
+    private String  role;
+    private boolean enabled;
+    private String  imageUrl;
+    @Column(length = 500)
+    private String  about;
+    
+}
---
+++ src/main/java/com/example/SmartContactManager/SmartContactManagerApplication.java
@@ -0,0 +1,17 @@
+package com.example.SmartContactManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartContactManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartContactManagerApplication.class, args);
+
+
+		System.out.println("Hi Bro I am UP............");
+		System.out.println("Nice to See you...........");
+	}
+
+}
