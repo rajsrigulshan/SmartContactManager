43fb0c0c7ecce8b1b45bd28c0a6bcc695f6c69cd
Raj Sri Gulshan <rajgulshan20022000@gmail.com>
2025-03-07 17:18:04 +0530

    server initialized

---
+++ Server/smartContactManagerServer/src/main/java/com/smartcontactmanager/smartContactManagerServer/SmartContactManagerServerApplication.java
@@ -0,0 +1,15 @@
+package com.smartcontactmanager.smartContactManagerServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartContactManagerServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartContactManagerServerApplication.class, args);
+
+		System.out.println("hi bro ...");
+	}
+
+}
