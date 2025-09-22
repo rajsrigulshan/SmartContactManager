32825135f085eabe4c25a6dc9367fb402d9b9be3
Raj Sri Gulshan <rajgulshan20022000@gmail.com>
2025-09-23 00:34:28 +0530

    cors configuration of server done.

--- Server/smartContactManagerServer/src/main/java/com/smartcontactmanager/smartContactManagerServer/controller/Homecontroller.java
+++ Server/smartContactManagerServer/src/main/java/com/smartcontactmanager/smartContactManagerServer/controller/Homecontroller.java
@@ -4,7 +4,6 @@ package com.smartcontactmanager.smartContactManagerServer.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,7 +20,6 @@ import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/home")
-@CrossOrigin(origins = "http://localhost:5173")
 public class Homecontroller {
 
     @Autowired
